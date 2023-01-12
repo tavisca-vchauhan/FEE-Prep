6252d4223e4a2bbad16b050ab7984a980fe4498b
Vishwas Chauhan <vishc64@gmail.com>
2023-01-12 12:26:59 +0530

    add routing & rxjs of-operator learnings

--- forms/my-forms/src/app/app-routing.module.ts
+++ forms/my-forms/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveForm, TemplateDrivenForm } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive-form',
+    component: ReactiveForm,
+  },
+  {
+    path: 'template-driven-form',
+    component: TemplateDrivenForm,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
