2f34604988ba0b7906f7866487de409c0a847053
Vishwas Chauhan <vishc64@gmail.com>
2023-01-13 19:45:08 +0530

    add debounceTime from take fromEvent operators learning

--- Rxjs/rxjs-learning/src/app/app.module.ts
+++ Rxjs/rxjs-learning/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OfComponent } from './components/of/of.component';
+import { FromComponent } from './components/from/from.component';
+import { FromEventComponent } from './components/from-event/from-event.component';
+import { DebounceTimeComponent } from './components/debounce-time/debounce-time.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TakeComponent } from './components/take/take.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OfComponent
+    OfComponent,
+    FromComponent,
+    FromEventComponent,
+    DebounceTimeComponent,
+    TakeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [FormsModule, ReactiveFormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
