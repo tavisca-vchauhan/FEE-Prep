f7253df1e0c02a17967779167111d90915f93738
Vishwas Chauhan <vishc64@gmail.com>
2023-02-10 16:14:41 +0530

    Add auth guard for user / products routes

--- ngRx/first-project/apps/shared/services/auth-guard.service.ts
+++ ngRx/first-project/apps/shared/services/auth-guard.service.ts
@@ -2,12 +2,26 @@ import { Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
 import { AppState } from '@shared/store/state';
 import { userLoginStatus } from '@shared//store/selectors';
+import { UrlTree, Router, CanActivate } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard {
-  constructor(private store: Store<AppState>) {}
+export class AuthGuard implements CanActivate {
+  constructor(private store: Store<AppState>, private router: Router) {}
 
-  canActivate(next: any, state: AppState) {
-    return this.store.select(userLoginStatus).subscribe((status) => status);
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | boolean
+    | UrlTree
+    | Promise<boolean | UrlTree> {
+    return this.store.select(userLoginStatus).pipe(
+      map((isUserLoggedIn) => {
+        if (!isUserLoggedIn) {
+          return this.router.createUrlTree(['login']);
+        } else {
+          return true;
+        }
+      })
+    );
   }
 }
